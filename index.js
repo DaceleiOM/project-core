75179aae859044d97fadb0e4258dc5e7dfdbcbae
DaceleiOM <168138499+DaceleiOM@users.noreply.github.com>
2024-08-19 13:12:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-require('dotenv-flow').config()
-const compression = require('compression')
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const apiRouter = require('./src/routes/index')
+const compression = require('compression');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const apiRouter = require('./src/routes/index');
 
-const app = express()
-const port = process.env.PORT || 3000
+// Configura Firebase y otras dependencias aquí
 
-if (process.env.ENV === 'local') process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+const app = express();
+const port = process.env.PORT || 3000;
 
-require('./src/database/config')
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(compression())
-
-// Routes
-app.use('/api', apiRouter)
-
-// Say hello
-app.get('/', (req, res) => (
-  res.status(200).json({ message: 'core en linea!' })
-))
-
-// Local environment
+// Carga configuración solo en local
 if (process.env.ENV === 'local') {
-  app.listen(port, () => {
-    console.log(`Servidor en :${port}`)
-  })
-} else { // cloud function
-  exports.core = app
+  require('dotenv').config(); // Solo carga .env en local
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 }
+
+require('./src/database/config');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression());
+
+// Rutas
+app.use('/api', apiRouter);
+
+// Ruta principal
+app.get('/', (req, res) => (
+  res.status(200).json({ message: 'core en línea!' })
+));
+
+// Exporta la aplicación para que Vercel pueda usarla como una función
+module.exports = app;
